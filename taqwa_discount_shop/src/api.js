5243c7591c33edbdf454f0ab123b10b76581be6e
maria <ritukhan534@gmail.com>
2026-09-03 11:37:47 +0600

    Fix production API URL

--- taqwa_discount_shop/src/api.js
+++ taqwa_discount_shop/src/api.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "https://shop-taqwa-react-fastapi-2.onrender.com",
+  baseURL: "https://shop-taqwa-react-fastapi-2.onrender.com",
 });
