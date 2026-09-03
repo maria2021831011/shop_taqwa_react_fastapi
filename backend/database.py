7bc6c94d2dd9f586701dcbfdc570fba4bf8c98a7
maria <ritukhan534@gmail.com>
2026-09-03 09:57:06 +0600

    Fix TiDB SSL connection

--- backend/database.py
+++ backend/database.py
@@ -5,13 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_connection():
-    """
-    Returns a MySQL connection object using pymysql.
-    """
     return pymysql.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        user=os.getenv("DB_USER", "root"),
-        password=os.getenv("DB_PASSWORD", ""),
-        database=os.getenv("DB_NAME", "taqwa_discount_shop"),
-        cursorclass=pymysql.cursors.DictCursor
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", 4000)),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        cursorclass=pymysql.cursors.DictCursor,
+        ssl={"ssl": {}}
     )
